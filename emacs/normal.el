a4b227ced95ec737e35887988d9571264f3e7463
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-08-30 12:47:22 -0300

    Update Normal

--- emacs/normal.el
+++ emacs/normal.el
@@ -1,8 +1,13 @@
+;; Initial
 (setq initial-major-mode 'fundamental-mode)
 (setq initial-scratch-message 'nil)
 (require 'uniquify)
+
+;; Server
 (require 'server)
 (unless (server-running-p) (server-start))
+
+;; Variables
 (custom-set-variables
  '(column-number-mode t)
  '(global-display-line-numbers-mode t)
@@ -15,3 +20,9 @@
  '(display-battery-mode t)
  '(display-time-mode t)
  '(xterm-mouse-mode 1))
+
+;; Default
+(setq-default font-lock-maximum-decoration t)
+(setq-default make-backup-files nil)
+(setq-default visible-bell t)
+(setq-default tab-width 4)
